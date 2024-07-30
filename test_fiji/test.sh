8a8f22019cb67f5ccb08413ff1f94a130e9e7f20
Cocopyth <corentin@bisot.com>
2024-07-30 16:06:35 +0200

    fixed binding of path through container

--- test_fiji/test.sh
+++ test_fiji/test.sh
@@ -5,5 +5,5 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --partition=rome
 #SBATCH --time=01:00:00
-apptainer exec --pwd $PWD $CODE_BASE_PATH/myimage.sif /bin/bash -c "$CODE_BASE_PATH/test_fiji/run_fiji.sh; echo 'Fiji has finished running'" 
+apptainer exec --bind $PATH_WHERE_TO_DOWNLOAD:$PATH_WHERE_TO_DOWNLOAD $CODE_BASE_PATH/myimage.sif /bin/bash -c "$CODE_BASE_PATH/test_fiji/run_fiji.sh; echo 'Fiji has finished running'" 
 
